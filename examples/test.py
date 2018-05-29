4e25440e1b116b7362951591e2b73a4fa6c899b5
Mohsen Naghipourfar <mn7697np@gmail.com>
2018-05-29 22:55:27 +0430

    Add Example for testing GPU

---
+++ examples/test.py
@@ -0,0 +1,8 @@
+from keras import backend as K
+"""
+    Created by Mohsen Naghipourfar on 5/29/18.
+    Email : mn7697np@gmail.com
+    Website: http://ce.sharif.edu/~naghipourfar
+"""
+
+K.tensorflow_backend._get_available_gpus()
